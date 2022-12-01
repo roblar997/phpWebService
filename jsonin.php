0f9fefd5e5e25e1879598219030a5d313235e197
s349967 <78033443+s349967@users.noreply.github.com>
2022-12-01 23:46:18 +0100

    Update jsonin.php

--- jsonin.php
+++ jsonin.php
@@ -1,7 +1,9 @@
+
 <?php
 try {
 $con=mysqli_connect('localhost','s349967','','s349967');
-if(!(isset($_REQUEST['lat']) && isset($_REQUEST['lng']) && isset($gateadresseInp=$_REQUEST['gateadresse'])&&isset($beskrivelseInp=$_REQUEST['beskrivelse']))){
+
+if(!(isset($_REQUEST['lat']) && isset($_REQUEST['lng']) && isset($_REQUEST['gateadresse'])&&isset($_REQUEST['beskrivelse']))){
     header("HTTP/1.1  400 Bad Request");
     print(json_encode("Bad Request"));
     mysqli_close($con);
@@ -18,9 +20,18 @@ if(!(isset($_REQUEST['lat']) && isset($_REQUEST['lng']) && isset($gateadresseInp
     $gateadresse=(string)$gateadresseInp;
     $beskrivelse=(string)$beskrivelseInp;
 
-    $sql=mysqli_query($con,"insert into s349967.Severdighet (lng,lat,gateadresse,beskrivelse) values('$lng','$lat','$gateadresse','$beskrivelse');");
+
+
+   $sql=mysqli_query($con,"UPDATE s349967.Severdighet SET gateadresse='$gateadresse',beskrivelse='$beskrivelse' WHERE lat='$lat' AND lng='$lng';");
+   if($sql){
     header("HTTP/1.1 200 OK");
     print(json_encode("OK"));
+   }
+   else {
+    header("HTTP/1.1 404 Not Found");
+    print(json_encode("Not Found"));
+   }
+
 }
 catch(Exception $e) {
     header("HTTP/1.1  400 Bad Request");
