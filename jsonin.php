53b6c83bc9cac0eb55374e0d2bb3aa135d278f42
s349967 <78033443+s349967@users.noreply.github.com>
2022-12-01 21:46:34 +0100

    Update jsonin.php

--- jsonin.php
+++ jsonin.php
@@ -1,16 +1,29 @@
 <?php
 try {
 $con=mysqli_connect('localhost','s349967','','s349967');
-$latInp=$_REQUEST['lat'];
-$lngInp=$_REQUEST['lng'];
-$gateadresseInp=$_REQUEST['gateadresse'];
-$beskrivelseInp=$_REQUEST['beskrivelse'];
+if(!(isset($_REQUEST['lat']) && isset($_REQUEST['lng']) && isset($gateadresseInp=$_REQUEST['gateadresse'])&&isset($beskrivelseInp=$_REQUEST['beskrivelse']))){
+    header("HTTP/1.1  400 Bad Request");
+    print(json_encode("Bad Request"));
+    mysqli_close($con);
+    return;
+}
 
-$lng=(float) $lngInp;
-$lat=(float) $latInp;
-$gateadresse=(string)$gateadresseInp;
-$beskrivelse=(string)$beskrivelseInp;
+    $latInp=$_REQUEST['lat'];
+    $lngInp=$_REQUEST['lng'];
+    $gateadresseInp=$_REQUEST['gateadresse'];
+    $beskrivelseInp=$_REQUEST['beskrivelse'];
+    
+    $lng=(float) $lngInp;
+    $lat=(float) $latInp;
+    $gateadresse=(string)$gateadresseInp;
+    $beskrivelse=(string)$beskrivelseInp;
 
+catch(Exception ex){
+    header("HTTP/1.1  400 Bad Request");
+    print(json_encode("Bad Request"));
+    mysqli_close($con);
+    return;
+}
    $sql=mysqli_query($con,"insert into s349967.Severdighet (lng,lat,gateadresse,beskrivelse) values('$lng','$lat','$gateadresse','$beskrivelse');");
    header("HTTP/1.1 200 OK");
    print(json_encode("OK"));
